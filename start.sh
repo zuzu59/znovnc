8b9da40a5c06b1c30b55c29e2fbc6fd56ba1d706
Christian Zufferey <christian@zufferey.com>
2019-10-11 17:34:51 +0200

    Amélioré mon script start.sh :-)

--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 #Petit script pour démarrer le binz
-#zf191011.1724
+#zf191011.1734
 
 # source: https://hub.docker.com/r/dougw/novnc
 
-docker run -e REMOTE_HOST=128.178.1.196 -e REMOTE_PORT=5900 -p 8081:8081 dougw/novnc
+
+#test si l'argument est vide
+if [ -z "$1" ]
+  then
+    echo -e "\nUsage: ./start.sh.sh adresse ip du serveur VNC remote \n\n"
+    exit
+fi
+
+
+
+docker run -e REMOTE_HOST=$1 -e REMOTE_PORT=5900 -p 8081:8081 dougw/novnc
 
